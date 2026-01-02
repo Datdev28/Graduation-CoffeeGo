1860e24e8e3c160f8c3ea133563c2f6ee609907f
Datdev28 <nguyenquangdat2842003@gmail.com>
2026-01-02 23:21:18 +0700

    frontend: fix socket url

--- frontend/src/page/admin/Orders.jsx
+++ frontend/src/page/admin/Orders.jsx
@@ -49,7 +49,7 @@ export default function Orders() {
 
   // Socket setup 
   useEffect(() => {
-    const socket = io("http://localhost:5000");
+    const socket = io("https://coffee-go.onrender.com");
 
     socket.on("connect", () => {
       socket.emit("join_admin");
